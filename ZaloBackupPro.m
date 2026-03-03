344462bc1f5695385049824604014086b6f0c1ae
hungbvtv <hungkhongbiet1998@gmail.com>
2026-03-03 23:21:46 +0700

    Update ZaloBackupPro.m

--- ZaloBackupPro.m
+++ ZaloBackupPro.m
@@ -1,13 +1,10 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
 
-// --- Giao diện Quản lý ---
-@interface ZBManager : NSObject <UIDocumentPickerDelegate>
+@interface ZBManager : NSObject
 + (instancetype)shared;
-- (void)startBackup;
-- (void)startRestore;
-- (UIViewController *)getTopVC;
+- (void)startBackupFrom:(UIViewController *)vc;
+- (void)startRestoreFrom:(UIViewController *)vc;
 @end
 
 @implementation ZBManager
@@ -16,118 +13,169 @@
     dispatch_once(&t, ^{ s = [ZBManager new]; }); return s;
 }
 
-- (UIViewController *)getTopVC {
-    UIWindow *keyWindow = nil;
-    for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
-        if (scene.activationState == UISceneActivationStateForegroundActive && [scene isKindOfClass:[UIWindowScene class]]) {
-            for (UIWindow *window in ((UIWindowScene *)scene).windows) {
-                if (window.isKeyWindow) { keyWindow = window; break; }
-            }
-        }
-    }
-    if (!keyWindow) keyWindow = UIApplication.sharedApplication.windows.firstObject;
-    UIViewController *top = keyWindow.rootViewController;
-    while (top.presentedViewController) top = top.presentedViewController;
-    return top;
+- (NSString *)backupRoot {
+    return [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject] stringByAppendingPathComponent:@"ZaloBackupPro_Data"];
 }
 
-- (void)startBackup {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"ZaloBackup Pro" message:@"Bạn muốn lưu bản backup vào đâu?" preferredStyle:UIAlertControllerStyleActionSheet];
-    [alert addAction:[UIAlertAction actionWithTitle:@"Lưu vào App Documents" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        [self runBackupToPath:nil];
+- (void)startBackupFrom:(UIViewController *)vc {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"ZaloBackup Pro" message:@"Bắt đầu sao lưu Tin nhắn & Hình ảnh?" preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"Sao Lưu Ngay" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self runBackupFrom:vc];
     }]];
     [alert addAction:[UIAlertAction actionWithTitle:@"Hủy" style:UIAlertActionStyleCancel handler:nil]];
-    [[self getTopVC] presentViewController:alert animated:YES completion:nil];
+    [vc presentViewController:alert animated:YES completion:nil];
 }
 
-- (void)runBackupToPath:(NSString *)customPath {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+- (void)runBackupFrom:(UIViewController *)vc {
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
         NSFileManager *fm = NSFileManager.defaultManager;
-        NSString *docPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-        NSString *backupDir = [docPath stringByAppendingPathComponent:@"ZaloBackupPro_Data"];
+        NSString *dest = [self backupRoot];
+        [fm createDirectoryAtPath:dest withIntermediateDirectories:YES attributes:nil error:nil];
         
-        [fm createDirectoryAtPath:backupDir withIntermediateDirectories:YES attributes:nil error:nil];
+        NSString *home = NSHomeDirectory();
+        NSArray *searchPaths = @[
+            [home stringByAppendingPathComponent:@"Library/Application Support"],
+            [home stringByAppendingPathComponent:@"Documents"]
+        ];
         
-        NSString *libPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Library/Application Support"];
-        NSInteger count = 0;
+        // Các định dạng cần backup
+        NSSet *exts = [NSSet setWithArray:@[@"db", @"sqlite", @"sqlite-wal", @"sqlite-shm", @"jpg", @"png", @"mp4", @"mov"]];
+        __block NSInteger count = 0;
         
-        NSDirectoryEnumerator *enumerator = [fm enumeratorAtPath:libPath];
-        NSString *file;
-        while ((file = [enumerator nextObject])) {
-            if ([file.pathExtension isEqualToString:@"db"] || [file.pathExtension isEqualToString:@"sqlite"]) {
-                NSString *src = [libPath stringByAppendingPathComponent:file];
-                NSString *dst = [backupDir stringByAppendingPathComponent:[file stringByReplacingOccurrencesOfString:@"/" withString:@"_"]];
-                [fm removeItemAtPath:dst error:nil];
-                if ([fm copyItemAtPath:src toPath:dst error:nil]) count++;
+        for (NSString *path in searchPaths) {
+            NSDirectoryEnumerator *en = [fm enumeratorAtPath:path];
+            NSString *file;
+            while ((file = [en nextObject])) {
+                if ([exts containsObject:file.pathExtension.lowercaseString]) {
+                    NSString *src = [path stringByAppendingPathComponent:file];
+                    // Chuyển dấu / thành __ để lưu phẳng trong thư mục backup
+                    NSString *safeName = [file stringByReplacingOccurrencesOfString:@"/" withString:@"__"];
+                    NSString *dst = [dest stringByAppendingPathComponent:safeName];
+                    [fm removeItemAtPath:dst error:nil];
+                    if ([fm copyItemAtPath:src toPath:dst error:nil]) count++;
+                }
             }
         }
         
         dispatch_async(dispatch_get_main_queue(), ^{
-            UIAlertController *done = [UIAlertController alertControllerWithTitle:@"Thành công" message:[NSString stringWithFormat:@"Đã backup %ld tệp tin database.", (long)count] preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertController *done = [UIAlertController alertControllerWithTitle:@"Thành công" message:[NSString stringWithFormat:@"Đã sao lưu %ld tệp tin.", (long)count] preferredStyle:UIAlertControllerStyleAlert];
             [done addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
-            [[self getTopVC] presentViewController:done animated:YES completion:nil];
+            [vc presentViewController:done animated:YES completion:nil];
         });
     });
 }
 
-- (void)startRestore {
-    // Logic khôi phục tương tự
+- (void)startRestoreFrom:(UIViewController *)vc {
+    NSFileManager *fm = NSFileManager.defaultManager;
+    NSString *srcDir = [self backupRoot];
+    NSArray *files = [fm contentsOfDirectoryAtPath:srcDir error:nil];
+    
+    if (files.count == 0) {
+        UIAlertController *err = [UIAlertController alertControllerWithTitle:@"Lỗi" message:@"Không tìm thấy dữ liệu để khôi phục." preferredStyle:UIAlertControllerStyleAlert];
+        [err addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        [vc presentViewController:err animated:YES completion:nil];
+        return;
+    }
+
+    UIAlertController *res = [UIAlertController alertControllerWithTitle:@"Khôi Phục" message:@"Dữ liệu hiện tại sẽ bị ghi đè. Bạn có chắc chắn?" preferredStyle:UIAlertControllerStyleAlert];
+    [res addAction:[UIAlertAction actionWithTitle:@"Khôi Phục Ngay" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            for (NSString *f in files) {
+                NSString *src = [srcDir stringByAppendingPathComponent:f];
+                // Chuyển __ ngược lại thành / để tìm đường dẫn gốc
+                NSString *relPath = [f stringByReplacingOccurrencesOfString:@"__" withString:@"/"];
+                NSString *dst = [[NSHomeDirectory() stringByAppendingPathComponent:@"Library/Application Support"] stringByAppendingPathComponent:relPath];
+                
+                [fm createDirectoryAtPath:dst.stringByDeletingLastPathComponent withIntermediateDirectories:YES attributes:nil error:nil];
+                [fm removeItemAtPath:dst error:nil];
+                [fm copyItemAtPath:src toPath:dst error:nil];
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                UIAlertController *done = [UIAlertController alertControllerWithTitle:@"Xong" message:@"Đã khôi phục dữ liệu. Vui lòng đóng hẳn Zalo và mở lại." preferredStyle:UIAlertControllerStyleAlert];
+                [done addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+                [vc presentViewController:done animated:YES completion:nil];
+            });
+        });
+    }]];
+    [res addAction:[UIAlertAction actionWithTitle:@"Hủy" style:UIAlertActionStyleCancel handler:nil]];
+    [vc presentViewController:res animated:YES completion:nil];
 }
 @end
 
-// --- Cửa sổ Nút bấm Nổi ---
 @interface ZBWindow : UIWindow
-@property (nonatomic, strong) UIButton *floatingBtn;
+@property (nonatomic, strong) UIButton *btn;
 @end
 
 @implementation ZBWindow
 - (instancetype)init {
-    self = [super initWithFrame:CGRectMake(20, 150, 60, 60)];
+    self = [super initWithFrame:[UIScreen mainScreen].bounds];
     if (self) {
+        self.windowLevel = UIWindowLevelAlert + 1000;
         self.backgroundColor = [UIColor clearColor];
-        self.windowLevel = UIWindowLevelAlert + 999; // Luôn trên cùng
+        self.rootViewController = [UIViewController new];
         
-        // Hỗ trợ iOS 13 đến iOS 26+
-        if (@available(iOS 13.0, *)) {
-            for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
-                if ([scene isKindOfClass:[UIWindowScene class]] && scene.activationState == UISceneActivationStateForegroundActive) {
-                    self.windowScene = (UIWindowScene *)scene; break;
-                }
-            }
-        }
-
-        self.floatingBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        self.floatingBtn.frame = self.bounds;
-        self.floatingBtn.backgroundColor = [UIColor colorWithRed:0.0 green:0.5 blue:1.0 alpha:0.8];
-        self.floatingBtn.layer.cornerRadius = 30;
-        [self.floatingBtn setTitle:@"ZBackup" forState:UIControlStateNormal];
-        self.floatingBtn.titleLabel.font = [UIFont systemFontOfSize:10 weight:UIFontWeightBold];
-        [self.floatingBtn addTarget:self action:@selector(btnClicked) forControlEvents:UIControlEventTouchUpInside];
+        self.btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        self.btn.frame = CGRectMake(20, 150, 60, 60);
+        self.btn.backgroundColor = [UIColor colorWithRed:0 green:0.5 blue:1 alpha:0.8];
+        self.btn.layer.cornerRadius = 30;
+        [self.btn setTitle:@"ZPRO" forState:UIControlStateNormal];
+        self.btn.titleLabel.font = [UIFont boldSystemFontOfSize:12];
+        [self.btn addTarget:self action:@selector(btnTapped) forControlEvents:UIControlEventTouchUpInside];
         
-        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
-        [self.floatingBtn addGestureRecognizer:pan];
-        
-        [self addSubview:self.floatingBtn];
-        self.hidden = NO;
+        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
+        [self.btn addGestureRecognizer:pan];
+        [self addSubview:self.btn];
     }
     return self;
 }
 
-- (void)btnClicked {
-    [[ZBManager shared] startBackup];
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    UIView *view = [super hitTest:point withEvent:event];
+    if (view == self.btn || self.rootViewController.presentedViewController) return view;
+    return nil;
 }
 
-- (void)handlePan:(UIPanGestureRecognizer *)pan {
-    CGPoint translation = [pan translationInView:self];
-    self.center = CGPointMake(self.center.x + translation.x, self.center.y + translation.y);
-    [pan setTranslation:CGPointZero inView:self];
+- (void)btnTapped {
+    UIAlertController *menu = [UIAlertController alertControllerWithTitle:@"ZaloBackup Pro" message:@"Chọn chức năng" preferredStyle:UIAlertControllerStyleActionSheet];
+    [menu addAction:[UIAlertAction actionWithTitle:@"Backup (Sao lưu)" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
+        [[ZBManager shared] startBackupFrom:self.rootViewController];
+    }]];
+    [menu addAction:[UIAlertAction actionWithTitle:@"Restore (Khôi phục)" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
+        [[ZBManager shared] startRestoreFrom:self.rootViewController];
+    }]];
+    [menu addAction:[UIAlertAction actionWithTitle:@"Đóng" style:UIAlertActionStyleCancel handler:nil]];
+    
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        menu.popoverPresentationController.sourceView = self.btn;
+    }
+    [self.rootViewController presentViewController:menu animated:YES completion:nil];
+}
+
+- (void)pan:(UIPanGestureRecognizer *)p {
+    CGPoint t = [p translationInView:self];
+    self.btn.center = CGPointMake(self.btn.center.x + t.x, self.btn.center.y + t.y);
+    [p setTranslation:CGPointZero inView:self];
+}
+
+- (void)update {
+    if (@available(iOS 13.0, *)) {
+        for (UIWindowScene *s in UIApplication.sharedApplication.connectedScenes) {
+            if (s.activationState == UISceneActivationStateForegroundActive) { self.windowScene = s; break; }
+        }
+    }
 }
 @end
 
-static ZBWindow *globalWin;
-__attribute__((constructor)) static void load() {
-    // Chờ 5 giây để Zalo load xong Scene trên iOS đời cao
+static ZBWindow *win;
+__attribute__((constructor)) static void init() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        globalWin = [[ZBWindow alloc] init];
+        win = [[ZBWindow alloc] init];
+        [win update];
+        win.hidden = NO;
+        [NSTimer scheduledTimerWithTimeInterval:3.0 repeats:YES block:^(NSTimer *t) {
+            [win update];
+            [win makeKeyAndVisible];
+            win.hidden = NO;
+        }];
     });
 }
