8d70be216ca05b82ff3566a36b23671c153b5a74
hungbvtv <hungkhongbiet1998@gmail.com>
2026-03-04 07:22:00 +0700

    Update ZaloBackupPro.m

--- ZaloBackupPro.m
+++ ZaloBackupPro.m
@@ -288,7 +288,7 @@
     [menu addAction:[UIAlertAction actionWithTitle:@"Restore" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
         [[ZBManager shared] startRestoreFrom:self.rootVC];
     }]];
-    [menu addAction:[UIAlertAction actionWithTitle:@"Dong" style:UIAlertActionStyleCancel handler:nil]];
+    [menu addAction:[UIAlertAction actionWithTitle:@"Đóng" style:UIAlertActionStyleCancel handler:nil]];
     if (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad) {
         menu.popoverPresentationController.sourceView = self.btn;
         menu.popoverPresentationController.sourceRect = self.btn.bounds;
